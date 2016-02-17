95192cea7f9dc2e419d37ab2b4adcebdc6d03008
Ryan Lane <ryan.lane@lab.coop>
2016-02-17 11:12:34 +0100

    Added labelText and <label> styles to async-data example

--- examples/async-data/app.js
+++ examples/async-data/app.js
@@ -24,6 +24,7 @@ let App = React.createClass({
         </p>
 
         <Autocomplete
+          labelText="Choose a state fromm the US"
           ref="autocomplete"
           items={this.state.unitedStates}
           getItemValue={(item) => item.name}
