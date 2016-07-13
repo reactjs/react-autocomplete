reactjs/react-autocomplete

commit 3f83a9229b1ad8efd3cc61224a17a9d769a429ed

[fixed] Select highlighted item on input click (#84)
While trying to debug #80 I discovered that clicking the input field to
"confirm" a suggestion didn't work as expected.

The problem:

1. Navigate to https://reactcommunity.org/react-autocomplete/static-data/
2. Type 'a' in input field
3. Observe that the input has been filled with 'Alabama'
4. Click anywhere inside the input field
5. Observe that the highlighted selection is gone, and the menu has been closed
6. Click/tab outside the input
7. Observe that the input value has been reverted to 'a'

This change ensures that clicking in the input field has the same effect as
clicking the highlighted item in the suggestion menu.

lib/Autocomplete.js
@@ -305,6 +305,8 @@ let Autocomplete = React.createClass({
   handleInputClick () {
     if (this.isInputFocused() && this.state.isOpen === false)
       this.setState({ isOpen: true })
+    else if (this.state.highlightedIndex !== null)
+      this.selectItemFromMouse(this.getFilteredItems()[this.state.highlightedIndex])
   },
 
   render () {

lib/__tests__/Autocomplete-test.js
@@ -244,6 +244,27 @@ describe('Autocomplete kewDown->Escape event handlers', () => {
 
 });
 
+describe('Autocomplete click event handlers', () => {
+
+  var autocompleteWrapper = mount(AutocompleteComponentJSX({}));
+  var autocompleteInputWrapper = autocompleteWrapper.find('input');
+
+  it('should update input value from selected menu item and close the menu', () => {
+    autocompleteWrapper.setState({ isOpen: true });
+    autocompleteInputWrapper.simulate('focus');
+    autocompleteInputWrapper.simulate('change', { target: { value: 'Ar' } });
+    
+    // simulate keyUp of last key, triggering autocomplete suggestion + selection of the suggestion in the menu
+    autocompleteInputWrapper.simulate('keyUp', { key : 'r', keyCode: 82, which: 82 }); 
+
+    // Click inside input, updating state.value with the selected Autocomplete suggestion
+    autocompleteInputWrapper.simulate('click');
+    expect(autocompleteWrapper.state('value')).to.equal('Arizona');
+    expect(autocompleteWrapper.state('isOpen')).to.be.false;
+  });
+
+});
+
 // Component method unit tests
 describe('Autocomplete#renderMenu', () => {
 

commit 6e46038511f0a0130e4bcab9f8fa1a8df360c3ef

[fixed] Prevent menu from closing immediately when clicking into input
A bug introduced in #84.

lib/Autocomplete.js
@@ -301,6 +301,11 @@ let Autocomplete = React.createClass({
   handleInputFocus () {
     if (this._ignoreBlur)
       return
+    // We don't want `selectItemFromMouse` to trigger when
+    // the user clicks into the input to focus it, so set this
+    // flag to cancel out the logic in `handleInputClick`.
+    // The event order is:  MouseDown -> Focus -> MouseUp -> Click
+    this._ignoreClick = true
     this.setState({ isOpen: true })
   },
 
@@ -312,8 +317,9 @@ let Autocomplete = React.createClass({
   handleInputClick () {
     if (this.isInputFocused() && this.state.isOpen === false)
       this.setState({ isOpen: true })
-    else if (this.state.highlightedIndex !== null)
+    else if (this.state.highlightedIndex !== null && !this._ignoreClick)
       this.selectItemFromMouse(this.getFilteredItems()[this.state.highlightedIndex])
+    this._ignoreClick = false
   },
 
   render () {