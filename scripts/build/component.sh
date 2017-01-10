8394e9a8daa0495364bfd08c185b50ac23388d2c
Christian Tegnér <christian.tegner@gmail.com>
2017-01-10 14:13:58 +0100

    Replace rackt build with browserify

---
+++ scripts/build/component.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -e
+
+export NODE_ENV=production
+mkdir -p build/lib dist
+babel lib/{index,Autocomplete}.js -d build/
+cd dist
+browserify ../lib/Autocomplete.js \
+    --transform babelify \
+    --external react \
+    --external react-dom \
+    --debug \
+  | youemdee ReactAutocomplete \
+    --dependency react:React \
+    --dependency react-dom:ReactDOM \
+  | exorcist react-autocomplete.js.map \
+  > react-autocomplete.js
+uglifyjs react-autocomplete.js \
+    --compress \
+    --in-source-map react-autocomplete.js.map \
+    --source-map react-autocomplete.min.js.map \
+  > react-autocomplete.min.js
