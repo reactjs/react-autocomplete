cf78f756a5a5fba657996c6d0608f9fcaa14bc59
Christian Tegnér <christian.tegner@gmail.com>
2017-02-18 15:06:58 +0100

    Make package version available to README-template

--- bin/generate-readme.js
+++ bin/generate-readme.js
@@ -1,12 +1,14 @@
 'use strict'
 
 const fs = require('fs')
+const pkg = require('../package.json')
 const generateDocFromModule = require('./generate-api-doc.js')
 
 try {
   const API_DOC = generateDocFromModule(`${__dirname}/../lib/Autocomplete.js`)
   const readme = fs.readFileSync(`${__dirname}/../README-template.md`, 'utf-8')
     .replace(/\$\{API_DOC\}/g, API_DOC)
+    .replace(/\$\{VERSION\}/g, pkg.version)
   fs.writeFileSync(`${__dirname}/../README.md`, readme)
 } catch (e) {
   console.error(e)
