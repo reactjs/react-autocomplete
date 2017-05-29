c406c4fa84417e3e16b254a4a68bcb2d377b4d7b
Christian Tegnér <christian.tegner@gmail.com>
2017-05-29 18:35:06 +0200

    Replace deprecated React.PropTypes with prop-types module
    This will cause weird warnings for users still on React <=0.14.8.

--- scripts/build/component.sh
+++ scripts/build/component.sh
@@ -8,10 +8,12 @@ browserify ../lib/Autocomplete.js \
     --transform babelify \
     --external react \
     --external react-dom \
+    --external prop-types \
     --debug \
   | youemdee ReactAutocomplete \
     --dependency react:React \
     --dependency react-dom:ReactDOM \
+    --dependency prop-types:PropTypes \
   | exorcist react-autocomplete.js.map \
   > react-autocomplete.js
 uglifyjs react-autocomplete.js \
